ab6bed7dc638dac0895b05f83249819089e6c207
youss <youssefkaddb@gmail.com>
2025-09-29 09:40:05 +0200

    Subida de nuevos ejemplos vistos en clase y el progreso de la actividad3

---
+++ AccesoDatos/Ficheros/src/Actividad3/BusquedaDeTexto.java
@@ -0,0 +1,16 @@
+package Actividad3;
+
+import java.io.File;
+
+public class BusquedaDeTexto {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Indicar por favor nombre de fichero y la palabra que desea buscar");
+            return;
+        }
+        String fich = args [0];
+        String palabra = args [1];
+
+
+    }
+}
