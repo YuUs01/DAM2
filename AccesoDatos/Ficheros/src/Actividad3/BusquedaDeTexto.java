660064132a145363128f25ef10a353ee77e1434d
youss <youssefkaddb@gmail.com>
2025-09-29 10:04:36 +0200

    Subida de la actividad3 acabada de manera total

--- AccesoDatos/Ficheros/src/Actividad3/BusquedaDeTexto.java
+++ AccesoDatos/Ficheros/src/Actividad3/BusquedaDeTexto.java
@@ -1,6 +1,6 @@
 package Actividad3;
 
-import java.io.File;
+import java.io.*;
 
 public class BusquedaDeTexto {
     public static void main(String[] args) {
@@ -8,9 +8,29 @@ public class BusquedaDeTexto {
             System.out.println("Indicar por favor nombre de fichero y la palabra que desea buscar");
             return;
         }
+
         String fich = args [0];
+
         String palabra = args [1];
 
+        try (BufferedReader fbr = new BufferedReader(new FileReader(fich))){
+            int i = 0;
+            String linea = fbr.readLine();
 
+            while (linea != null){
+                i++;
+                if (linea.contains(palabra)){
+                    System.out.println("En la línea" + i + " se encuentra la palabra " + palabra);
+                }
+                linea = fbr.readLine();
+            }
+
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("No existe fichero " + fich);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
