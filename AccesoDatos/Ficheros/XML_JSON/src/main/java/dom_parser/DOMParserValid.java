069f925f4bfe933a81ba730851acd0646bc245cb
youss <youssefkaddb@gmail.com>
2025-10-09 09:18:08 +0200

    Adicion de la clase de ejemplo de validacion de xml

--- AccesoDatos/Ficheros/XML_JSON/src/main/java/dom_parser/DOMParserValid.java
+++ AccesoDatos/Ficheros/XML_JSON/src/main/java/dom_parser/DOMParserValid.java
@@ -4,6 +4,7 @@ package dom_parser;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
  */
 
 import java.io.File;
