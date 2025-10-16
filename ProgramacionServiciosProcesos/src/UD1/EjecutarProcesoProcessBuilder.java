5ac6beedb8386e024baae9488c215cfc92bbee5f
youss <youssefkaddb@gmail.com>
2025-10-16 08:28:30 +0200

    Subida dell segundo ejercicio en el que se usa el processbuilder para para hacer un ping a google

---
+++ ProgramacionServiciosProcesos/src/UD1/EjecutarProcesoProcessBuilder.java
@@ -0,0 +1,21 @@
+package UD1;
+
+import java.io.IOException;
+
+public class EjecutarProcesoProcessBuilder {
+    public static void main(String[] args) {
+
+        try{
+            ProcessBuilder pb = new ProcessBuilder("ping", "www.google.com");
+            Process proceso = pb.start();
+            int exitCode = proceso.waitFor();
+            System.out.println("Código de salida " + exitCode);
+        } catch (IOException ioe) {
+            System.err.println("No se pudo ejecutar el proceso. " +
+                    "verifica que tienes permisos o que el comando existe: " + ioe.getMessage());
+        }catch (Exception e){
+            System.err.println("Se produjo un error inesperado: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
