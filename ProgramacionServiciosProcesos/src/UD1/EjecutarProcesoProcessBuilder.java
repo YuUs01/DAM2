148b306b5aa086e4611c1b5224a8a39af31fa3e3
youss <youssefkaddb@gmail.com>
2025-11-25 09:25:43 +0100

    Adjustment for a cleaner reading

--- ProgramacionServiciosProcesos/src/UD1/EjecutarProcesoProcessBuilder.java
+++ ProgramacionServiciosProcesos/src/UD1/EjecutarProcesoProcessBuilder.java
@@ -8,13 +8,13 @@ public class EjecutarProcesoProcessBuilder {
         try{
             ProcessBuilder pb = new ProcessBuilder("notepad");
             Process proceso = pb.start();
+
             int exitCode = proceso.waitFor();
             System.out.println("Código de salida " + exitCode);
 
         } catch (IOException ioe) {
             System.err.println("No se pudo ejecutar el proceso. " +
                     "verifica que tienes permisos o que el comando existe: " + ioe.getMessage());
-
         }catch (Exception e){
             System.err.println("Se produjo un error inesperado: " + e.getMessage());
             e.printStackTrace();
